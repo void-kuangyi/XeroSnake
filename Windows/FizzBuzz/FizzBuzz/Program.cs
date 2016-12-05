e81f7d7d46b783e86bc454159e440ae51feeb1bd
unknown <kuangyi.xing@XLW-5CG4380ZRD.inside.xero.com>
2016-12-06 00:02:52 +1100

    game application

---
+++ Windows/FizzBuzz/FizzBuzz/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FizzBuzz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 100; i++)
+            {
+                
+
+                if (i%3 == 0 && i%5 == 0)
+                {
+                    Console.Write(",Fizz Buzz");
+                }
+                else if (i % 3 == 0)
+                {
+                    Console.Write(",Fizz");
+                }
+                else if (i % 5 == 0)
+                {
+                    Console.Write(",Buzz");
+                }
+                else
+                {
+                    Console.Write("," + i);
+                }
+           
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Windows/game/game/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int turn = 0;
+            Random rnd = new Random();
+            int num = rnd.Next(0, 100);
+
+            Console.Write("Enter the input : ");
+
+            while(true)
+            {
+                int input = int.Parse(Console.ReadLine());
+
+                turn++;
+
+                if (input == num)
+                {
+                    Console.WriteLine("your turn number is " + turn);
+                    break;
+                }
+                if (input < num)
+                {
+                    Console.WriteLine("input is smaller than the number");
+                }
+                else
+                {
+                    Console.WriteLine("input is larger than the number");
+                }
+
+                
+            }
+
+
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
