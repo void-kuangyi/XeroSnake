3bef4fcb7caa14417034cf03f33a5df88e47109b
unknown <kuangyi.xing@XLW-5CG4380ZRD.inside.xero.com>
2016-12-05 16:49:36 +1100

    some work

---
+++ Windows/Loops/Loops/Program.cs
@@ -0,0 +1,25 @@
+﻿using system;
+using system.collections.generic;
+using system.linq;
+using system.text;
+using system.threading.tasks;
+
+namespace loops
+{
+    class program
+    {
+        static void main(string[] args)
+        {
+            for (int i = 0; i <= 10; i++)
+            {
+                if (i % 2 != 0)
+                {
+                    console.writeline(i);
+                }
+            }
+
+            console.readline();
+            
+        }
+    }
+}
---
+++ Windows/SampleIfs/SampleIfs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SampleIfs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int originalValue = 1;
+
+            string[] collection = new string[100];
+
+
+            foreach (var item in collection)
+            {
+                item.ToLower();
+            }
+
+            if (true)
+            {
+
+            }
+
+        }
+    }
+}
