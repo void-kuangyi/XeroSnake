9f03f1ef3a9da509b8a864bab53e48ad0d1cdaf5
kennllanto <kennllanto@gmail.com>
2017-01-13 10:59:04 +1100

    Start Engine Class

---
+++ Snake/BusinessLayer/Engine.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLayer
+{
+    class Engine
+    {
+        private int mazeLength { get; set; }
+        private int mazeWidth { get; set; }
+        private int[][] mazeArray { get; }
+        private enum gameMode
+        {
+            basic
+        }
+        gameMode currentMde = gameMode.basic;
+
+
+        public Engine(int length, int width, int mode)
+        {
+            mazeLength = length;
+            mazeWidth = width;
+            initializeGame();
+        }
+
+        private int[][] initializeGame()
+        {
+            // new Maze
+            // new Snake
+            return mazeArray;
+        }
+    }
+}
