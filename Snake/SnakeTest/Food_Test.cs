6e017527dd9b3d7b2809bdcea2124a5f09b4fbe0
kennllanto <kennllanto@gmail.com>
2017-01-12 16:50:09 +1100

    Added Food class and Food unit test

---
+++ Snake/SnakeTest/Food_Test.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SnakeTest
+{
+    [TestClass]
+    public class Food_Test
+    {
+        [TestMethod]
+        public void generateFoodTestOutsideBoundaryXAxis()
+        {
+            BusinessLayer.Food food = new BusinessLayer.Food();
+
+            bool result = food.generateFood(-1, 40);
+
+            Assert.IsFalse(result, "Accepted Invalid X input");
+
+        }
+
+        [TestMethod]
+        public void generateFoodTestOutsideBoundaryYAxis()
+        {
+            BusinessLayer.Food food = new BusinessLayer.Food();
+
+            bool result = food.generateFood(26, -3);
+
+
+            Assert.IsFalse(result, "Accepted Invalid X input");
+
+        }
+
+        [TestMethod]
+        public void generateFoodTestInsideBoundaryYAxis()
+        {
+            BusinessLayer.Food food = new BusinessLayer.Food();
+
+            bool result = food.generateFood(26, 22);
+
+            Assert.IsTrue(result, "Wrong Return");
+
+        }
+    }
+}
