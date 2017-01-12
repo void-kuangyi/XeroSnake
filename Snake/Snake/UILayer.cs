2c415a1a60e11d952885b5499a4c89b2b9dec0ee
Andrew <andrew.w.duff92@gmail.com>
2017-01-12 16:14:53 +1100

    Updated UI

--- Snake/Snake/UILayer.cs
+++ Snake/Snake/UILayer.cs
@@ -10,8 +10,56 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World");
-            Console.ReadKey();
+            bool ExitGame = false;
+            string UserExitRequest;
+
+            do
+            {
+                int[,] Maze = new int[5, 5] { { 1, 1, 1, 1, 1 }, { 1, 6, 6, 6, 1 }, { 1, 6, 3, 6, 1 }, { 1, 6, 6, 6, 1 }, { 1, 1, 1, 1, 1 } };
+                Draw(Maze);
+
+
+
+                UserExitRequest = Console.ReadLine().ToString();
+                if (UserExitRequest == "Q" || UserExitRequest == "q")
+                {
+                    ExitGame = true;
+                }
+            }
+            while (ExitGame == false);
+
+        }
+
+        public static void Draw(int[,] DynamicMaze)
+        {
+            int rowLength = DynamicMaze.GetLength(0);
+            int colLength = DynamicMaze.GetLength(1);
+
+            for (int i = 0; i < rowLength; i++)
+            {
+                for (int j = 0; j < colLength; j++)
+                {
+                    switch (DynamicMaze[i, j])
+                    {
+                        case 1:
+                            Console.Write("*"); //border horizontal 
+                            break;
+                        case 3:
+                            Console.Write("0"); //snake head
+                            break;
+                        case 4:
+                            Console.Write("o"); //snake body
+                            break;
+                        case 5:
+                            Console.Write("@"); //food
+                            break;
+                        case 6:
+                            Console.Write("-"); //snake body
+                            break;
+                    }
+                }
+                Console.Write(Environment.NewLine);
+            }
         }
     }
 }
