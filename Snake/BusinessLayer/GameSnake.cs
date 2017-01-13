76a749adf1f03fabe72c09fc52ccf1d54408f296
unknown <abhid94@gmail.com>
2017-01-13 13:33:46 +1100

    Added functionality for snake movement taking into account if food has been eaten or not

--- Snake/BusinessLayer/GameSnake.cs
+++ Snake/BusinessLayer/GameSnake.cs
@@ -8,8 +8,9 @@ namespace BusinessLayer
 {
     class GameSnake
     {
-        private int currentDirection;  //direction the snake is facing
-        private int length;
+        enum Direction {right, down, left, up};
+        Direction directionFacing;
+        private int snakeLength;
         private List<Point> currentPosition;
 
 
@@ -17,8 +18,8 @@ namespace BusinessLayer
         //Method to create new snake
         public List<Point> createFirstSnake(int width, int height, int newLength)
         {
-
-            this.length = newLength;   
+            this.directionFacing = Direction.right;
+            this.snakeLength = newLength;   
             //create head node (head of snake)
             Point head = new BusinessLayer.Point(width / 2, height / 2);
             List<Point> snakePoints = new List<Point>();
@@ -26,7 +27,7 @@ namespace BusinessLayer
             //create rest of nodes (body of snake)
 
             int headX = head.returnX() - 1;
-            for (int x = 0; x < length; x++)
+            for (int x = 0; x < snakeLength; x++)
             {
                 Point temp = new BusinessLayer.Point(headX, head.returnY());
                 headX--;
@@ -37,6 +38,47 @@ namespace BusinessLayer
         }
 
 
+        public List<Point> snakeMove(int direction, Boolean hasEaten)
+        {
+            //make new list equal to position to previous state of snake
+            List<Point> snakeMoveList = currentPosition;
+            Point snakeTurn = new Point(0, 0);
 
-    }
+            switch(direction)
+            {
+                 case (int)Direction.right:
+                     snakeTurn.setX(snakeMoveList[0].returnX()+1);
+                     snakeTurn.setY(snakeMoveList[0].returnY());
+                     break;  
+                 case (int)Direction.down:
+                     snakeTurn.setX(snakeMoveList[0].returnX());
+                     snakeTurn.setY(snakeMoveList[0].returnY()-1);
+                     break;
+                 case (int)Direction.left:
+                     snakeTurn.setX(snakeMoveList[0].returnX()-1);
+                     snakeTurn.setY(snakeMoveList[0].returnY());
+                    break;
+                 case (int)Direction.up:
+                     snakeTurn.setX(snakeMoveList[0].returnX());
+                     snakeTurn.setY(snakeMoveList[0].returnY()+1);
+                     break;
+
+            }
+
+            if (hasEaten == true)
+            {
+                snakeMoveList.Insert(0, snakeTurn); //add new head with new direction to snake
+            } else
+            {
+                snakeMoveList.Insert(0, snakeTurn); //add new head with new direction to snake
+                snakeMoveList.RemoveAt(snakeMoveList.Count() - 1); //remove last element of snake
+            }
+  
+            //update current position
+            this.currentPosition = snakeMoveList;
+
+            return snakeMoveList;
+        }
+
+     }
 }
