548acd91442b337c41cdfe1575cfab7b37a4ca3c
shreyas.nagarajappa <shreyas.nagarajappa@xero.com>
2017-01-16 10:26:53 +1100

    Added method to reset score

--- Snake/SnakeTest/Score_Test.cs
+++ Snake/SnakeTest/Score_Test.cs
@@ -23,12 +23,24 @@ namespace SnakeTest
             int incrementValue1 = 30;
             int incrementValue2 = 30;
             int incrementedScore;
-            
+
             Score.incrementScore(incrementValue1);
             Score.incrementScore(incrementValue2);
             incrementedScore = Score.getScore();
 
             Assert.IsTrue(incrementedScore == (incrementValue1 + incrementValue2), "Error in incrementing score");
+        }
+
+        [TestMethod]
+        public void resetScore_Resets_The_Score()
+        {
+            int currentScore;
+            int NEWSCORE = 0;
+
+            Score.resetScore();
+            currentScore = Score.getScore();
+
+            Assert.IsTrue(currentScore == NEWSCORE, "Newscore not set to 0");
         }
     }
 }
