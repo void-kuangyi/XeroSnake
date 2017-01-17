14fb9ded79e3e66a865b1b5d18aa1761cbcf814b
KuangyiXing <u5817313@anu.edu.au>
2017-01-17 14:48:36 +1100

    Generate random obstacles in maze

--- Snake/BusinessLayer/maze.cs
+++ Snake/BusinessLayer/maze.cs
@@ -26,11 +26,21 @@ namespace BusinessLayer
 
         public int[,] CreateMaze()
         {
+            Generate_Basic_Maze();
+
+            Generate_All_Obstacles();
+            
+            return Maze;
+        }
+
+
+        public void Generate_Basic_Maze()
+        {
             for (int i = 0; i < height; i++)
             {
                 for (int j = 0; j < width; j++)
                 {
-                    Maze[i,j] = NO_BORDER;   
+                    Maze[i, j] = NO_BORDER;
                 }
 
             }
@@ -43,7 +53,7 @@ namespace BusinessLayer
                     for (int j = 0; j < width; j++)
                     {
                         Maze[i, j] = BORDER;
-                    } 
+                    }
                 }
 
                 //first and last colum of other rows should be marked as border
@@ -54,23 +64,41 @@ namespace BusinessLayer
                 }
             }
 
-
-            Generate_Horizon_Line_In_Maze(random_number.Next(0,height),random_number.Next(0,width),2);
-
-            Generate_Vertical_Line_In_Maze(random_number.Next(0, height), random_number.Next(0, width), 2);
-
-            Generate_Rectangle_In_Maze(random_number.Next(0,height),2,3,5);
-
-            return Maze;
         }
 
 
 
-        public void Generate_Horizon_Line_In_Maze(int PointX, int PointY, int LineLength)
+        public void Generate_All_Obstacles()
+        {
+            int Vertical_Line_Head_X = random_number.Next(0,height);
+            int Vertical_Line_Head_Y = random_number.Next(width/2,width);
+
+            Generate_Vertical_Line_Obstacle(Vertical_Line_Head_X,Vertical_Line_Head_Y,random_number.Next(0,height - Vertical_Line_Head_X));
+
+            int Horizon_Line_Head_X = random_number.Next(0,height);
+            int Horizon_Line_Head_Y = random_number.Next(0,width/2 - 1);
+
+            Generate_Horizon_Line_Obstacle(Horizon_Line_Head_X,Horizon_Line_Head_Y,random_number.Next(0,Horizon_Line_Head_Y));
+
+
+            int Rectangle_Head_X = random_number.Next(0, height);
+            int Rectangle_Head_Y = random_number.Next(0, width);
+
+            int Rectangle_width = random_number.Next(0,width - Rectangle_Head_Y);
+            int Rectangle_height = random_number.Next(0, height - Rectangle_Head_X);
+
+            Generate_Rectangle_Obstacle(Rectangle_Head_X, Rectangle_Head_Y, Rectangle_width, Rectangle_height);
+
+
+
+        }
+
+
+        public void Generate_Horizon_Line_Obstacle(int PointX, int PointY, int LineLength)
         {
             for (int i = 0; i < LineLength; i++)
             {
-                Maze[PointX, PointY + i] = BORDER;
+                Maze[PointX, PointY  - i] = BORDER;
 
             }
 
@@ -78,7 +106,7 @@ namespace BusinessLayer
 
 
 
-        public void Generate_Vertical_Line_In_Maze(int PointX, int PointY, int LineLength)
+        public void Generate_Vertical_Line_Obstacle(int PointX, int PointY, int LineLength)
         {
 
             for (int i = 0; i < LineLength; i++)
@@ -90,7 +118,7 @@ namespace BusinessLayer
 
 
 
-        public void Generate_Rectangle_In_Maze(int PointX,int PointY,int rectangle_width, int rectangle_height)
+        public void Generate_Rectangle_Obstacle(int PointX,int PointY,int rectangle_width, int rectangle_height)
         {
             for (int i = 0; i < rectangle_height; i++)
             {
