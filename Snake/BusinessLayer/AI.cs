d42e2da6e3466eee5a50d2df01fc50e4b555e190
unknown <abhid94@gmail.com>
2017-01-18 12:10:09 +1100

    Finished AI Class, created AI test class

---
+++ Snake/BusinessLayer/AI.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace BusinessLayer
+{
+    class AI
+    {
+        private int X;
+        private int Y;
+        public int XCoordinate
+        {
+            get
+            {
+                return X;
+            }
+        }
+
+        public int YCoordinate
+        {
+            get
+            {
+                return Y;
+            }
+        }
+
+        public AI()
+        {
+            X = -1;
+            Y = -1;
+        }
+
+        public bool SpawnAI(int XBorder, int YBorder)
+        {
+            if (XBorder < 0 || YBorder < 0)
+            {
+                return false;
+            }
+            this.X = RandomPoint(XBorder - 1);
+            this.Y = RandomPoint(YBorder - 1);
+
+            return true;
+        }
+
+        public int RandomPoint(int limit)
+        {
+            Random randomNumber = new Random();
+            return randomNumber.Next(limit);
+        }
+
+        public void MoveAI()
+        {
+            Random rnd = new Random();
+            int randomMove = rnd.Next(3);
+
+            switch (randomMove)
+            {
+                case (int)Direction.Right:
+                    Y = Y - 1;
+                    break;
+                case (int)Direction.Left:
+                    Y = Y + 1;
+                    break;
+                case (int)Direction.Up:
+                    X = X + 1;
+                    break;
+                case (int)Direction.Down:
+                    X = X - 1;
+                    break;
+            }
+        }
+    }
+}
---
+++ Snake/SnakeTest/AI_Test.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SnakeTest
+{
+    [TestClass]
+    class AI_Test
+    {
+        public void SpawnAI_initialise_true()
+        {
+           // BusinessLayer
+
+
+        }
+    }
+}
