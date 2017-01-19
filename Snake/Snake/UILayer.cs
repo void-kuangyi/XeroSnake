cb491c87f4a81891d6cffc915847ab8e047eedc7
shreyas.nagarajappa <shreyas.nagarajappa@xero.com>
2017-01-19 14:14:24 +1100

    Bug Fixes: 1. Can replay game now after game ends on score of zero. 2. Fixed one source of memory leak.

--- Snake/Snake/UILayer.cs
+++ Snake/Snake/UILayer.cs
@@ -6,9 +6,9 @@ namespace Snake
     class Program
     {
         private const int GameStepMilliseconds = 100;
+        private static KeyListner keyListner = new KeyListner();
 
         private static bool ExitGame = false;
-        private static string UserReplay;
 
         static void Main(string[] args)
         {
@@ -24,7 +24,6 @@ namespace Snake
                 score = Score.getScore();
                 drawScore(score);
 
-                KeyListner keyListner = new KeyListner(); 
                 ConsoleKeyInfo keyInfo = keyListner.ReadKey(GameStepMilliseconds);
                 switch (keyInfo.Key)
                 {
@@ -54,7 +53,6 @@ namespace Snake
                 }
                 System.Console.Clear();
                 Draw(UpdateMaze);
-                System.Threading.Thread.Sleep(1);
             }
             while (ExitGame == false);
 
@@ -94,12 +92,12 @@ namespace Snake
 
         static void endGame()
         {
-            System.Console.WriteLine("Your final score is " + Score.getScore());
-            System.Console.WriteLine("The high score is " + Score.getHighScore());
-            System.Console.WriteLine("Enter r to replay.");
-
-            UserReplay = Console.ReadLine().ToString();
-            if (UserReplay == "R" || UserReplay == "r")
+            Console.WriteLine("Your final score is " + Score.getScore());
+            Console.WriteLine("The high score is " + Score.getHighScore());
+            Console.WriteLine("Enter r to replay. Any other key to quit.");
+            
+            ConsoleKeyInfo keyInfo = keyListner.ReadKey(Int32.MaxValue);
+            if (keyInfo.KeyChar == 'R' || keyInfo.KeyChar == 'r')
             {
                 ExitGame = false;
                 Console.Clear();
