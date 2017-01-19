4603b8692c575e92c89d083e9bc4bd601c29ed4d
KuangyiXing <u5817313@anu.edu.au>
2017-01-19 14:30:41 +1100

    add live class

---
+++ Snake/BusinessLayer/Live.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BusinessLayer
+{
+    public class Live
+    {
+        private int liveNumber;
+
+        public Live()
+        {
+            liveNumber = 3;
+        }
+
+        public int LoseLive()
+        {
+            liveNumber--;
+            return liveNumber;
+
+        }
+
+        public bool IsDead()
+        {
+            return liveNumber == 0;
+
+        }
+
+
+
+    }
+}
