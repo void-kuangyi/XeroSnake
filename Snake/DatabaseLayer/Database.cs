23cba0127016559e260d4559a3fb05ba092ea9ad
shreyas.nagarajappa <shreyas.nagarajappa@xero.com>
2017-01-23 12:08:38 +1100

    Added database, db table, setup sql connection, and added query to read high scores

--- Snake/DatabaseLayer/Database.cs
+++ Snake/DatabaseLayer/Database.cs
@@ -1,26 +1,51 @@
 ﻿using System.IO;
+using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+using System.Collections.Generic;
 
 namespace DatabaseLayer
 {
     public class Database
     {
+        SqlConnection sqlConnection;
+        SqlCommand cmd;
+        SqlDataReader sqlDataReader;
+        string gameType;
+
+        public Database(string gameType)
+        {
+            sqlConnection = new SqlConnection("Server= localhost; Database= SnakeHighScores; Integrated Security=True;");
+            cmd = new SqlCommand();
+            cmd.CommandType = CommandType.Text;
+            cmd.Connection = sqlConnection;
+            this.gameType = gameType;
+        }
+
         const string FILENAME = "highscores.txt";
 
         public int getHighSCore()
         {
-            int highScore;
+            int highScore = 100;
 
-            if (File.Exists(FILENAME))
+            cmd.CommandText = "SELECT * FROM HighScores where GameType = '" + gameType + "'";
+
+            List<int> highScoreList = new List<int>();
+            List<string> nameList = new List<string>();
+
+            sqlConnection.Open();
+            sqlDataReader = cmd.ExecuteReader();
+
+            if (sqlDataReader.HasRows)
             {
-                using (BinaryReader reader = new BinaryReader(File.Open(FILENAME, FileMode.Open)))
+                while (sqlDataReader.Read())
                 {
-                    highScore = reader.ReadInt32();
+                    highScoreList.Add(sqlDataReader.GetInt32(2));
+                    nameList.Add(sqlDataReader.GetString(3));
                 }
             }
-            else
-            {
-                return 0;
-            }
+
+            sqlConnection.Close();
 
             return highScore;
         }
---
+++ Snake/DatabaseLayer/HighScores.cs
@@ -0,0 +1,10 @@
+﻿namespace DatabaseLayer
+{
+    class HighScores
+    {
+        public int Id { get; set; }
+        public string GameType { get; set; }
+        public string Name { get; set; }
+        public int Score { get; set; }
+    }
+}
