87faeb877892e0b08bd84c21bf2e32ac2789757c
shreyas.nagarajappa <shreyas.nagarajappa@xero.com>
2017-01-23 13:28:27 +1100

    Refactor sql script to reflect change in code

--- Create_HighScores_Table.sql
+++ Create_HighScores_Table.sql
@@ -1,6 +1,6 @@
 CREATE TABLE HighScores (
 ID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-GameType VARCHAR(13) NOT NULL CHECK (GameType IN('Basic', 'Intermediate', 'Advanced')),
+GameType VARCHAR(13) NOT NULL CHECK (GameType IN('Beginner', 'Easy', 'Medium', 'Hard')),
 Score INT NOT NULL,
 Name NVARCHAR(50)
 )
